9bfe63b5866683f5c26a0393c3f81a0b9f8fd96a
Agustín <aguszaku@gmail.com>
2026-03-26 22:32:34 -0300

    Parte 1 agregada - Acceso a elementos y recorrido de arrays

--- TRABAJO_PRACTICO_1/index.js
+++ TRABAJO_PRACTICO_1/index.js
@@ -7,3 +7,23 @@ const productos = [
 { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },  
 { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }  
 ]; 
+
+// Comando ejecución: node ./TRABAJO_PRACTICO_1/index.js
+// Parte 1: Acceso a elementos y recorrido de arrays ---------------- 
+
+// 1. Acceder a propiedades individuales:
+console.log(productos[0]);
+console.log("----------------------------------");
+
+// 2. Recorrido con for...of:
+console.log("Recorrido con for...of:");
+for (const prod of productos) {
+  console.log(`Nombre del producto: ${prod.nombre}, Precio: ${prod.precio}`);
+}
+console.log("----------------------------------");
+
+// 3. Recorrido con .forEach():
+console.log("Recorrido con .forEach():");
+productos.forEach((prod) => {
+  console.log(`Nombre del producto: ${prod.nombre}, Precio: ${prod.precio}`);
+})
